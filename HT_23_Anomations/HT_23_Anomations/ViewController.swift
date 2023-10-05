23876a0db3641973f600219c84147eecf0b31de9
AnastasiyaShved <pav159208@gmail.om>
2023-10-05 11:23:32 +0300

    created project

---
+++ HT_23_Anomations/HT_23_Anomations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HT_23_Anomations
+//
+//  Created by Apple on 5.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
